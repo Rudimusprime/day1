70aeca8f01ed0e0e465fb344830517eef4380c4a
Daniel Banasiewicz <d.banan1337@gmail.com>
2022-06-21 21:03:08 +0200

    Hello world

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {render} from "react-dom";
+
+function App() {
+    return (
+        <div className="root">
+            Hello World
+        </div>
+    )
+}
+
+export default App;
